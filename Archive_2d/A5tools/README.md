82783f4468cd9c956139aaa90a94a0e7d9e82fa5
hgoelzer <heiko@goelzers.de>
2019-10-21 22:56:22 +0200

    Add directory and scripts for 2d processing To extract snapshots and differences

---
+++ Archive_2d/A5tools/README.md
@@ -0,0 +1,26 @@
+## ismip6-gris-results-processing
+
+# A5tools
+
+### 2D calculations 
+
+## Initial model state end 2014
+- Ice mask
+- Surface elevation
+- Ice thickness
+- Bedrock
+- SMB
+- BMB
+- applied MB
+- Mass above flotation
+- Velocity magnitude
+- Log velocity magnitude
+
+## Projection (For all scenarios/models, including control). Snapshots at 2040, 2060 and 2100. Differences/changes relative to end 2014. 
+- Thickness change proj_ctrl 
+- Surface elevation change 
+- Bedrock change (if any)
+- SMB (2021-2040, 2041-2060, 2081-2100)
+- integrated dSMB (2015-2040, 2015-2060, 2015-2100)
+- dynamic contribution (Modelled thickness change - integrated dSMB, same dates)
+
---
+++ Archive_2d/A5tools/meta_2d_05.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+# Calculate 2d values for a number of models/experiments
+
+set -x
+set -e
+
+# location of Archive
+#outp=/Volumes/ISMIP6/ISMIP6-Greenland/Archive_05/Data
+outp=/home/hgoelzer/Projects/ISMIP6/Archive_05/Data
+
+# Destination for scalar files
+outpsc=/home/hgoelzer/Projects/ISMIP6/Archive_2d/Data
+
+## Settings
+ares=05
+
+#declare -a labs=(AWI)
+#declare -a models=(ISSM1)
+
+## labs/models lists
+#declare -a labs=(ILTS_PIK)
+#declare -a models=(SICOPOLIS2)
+
+# labs/models lists
+#declare -a labs=(IMAU)
+#declare -a models=(NOISM05)
+
+# labs/models lists
+declare -a labs=(JPL)
+declare -a models=(ISSM)
+
+# labs/models lists
+#declare -a labs=(MUN)
+#declare -a models=(GSM2371)
+
+# labs/models lists
+#declare -a labs=(UCIJPL)
+#declare -a models=(ISSM)
+
+# labs/models lists
+#declare -a labs=(VUB)
+#declare -a models=(GISMSIAv1)
+
+## labs/models lists
+#declare -a labs=(LSCE)
+#declare -a models=(GRISLI)
+
+
+
+
+# labs/models lists
+#declare -a labs=(LSCE)
+#declare -a models=(GRISLI)
+
+# labs/models lists
+#declare -a labs=(IMAU LSCE)
+#declare -a models=(IMAUICE2 GRISLI)
+
+# labs/models lists
+#declare -a labs=(ILTS_PIK IMAU LSCE)
+#declare -a models=(SICOPOLIS2 IMAUICE2 GRISLI)
+
+# labs/models lists
+#declare -a labs=(AWI AWI AWI ILTS_PIK ILTS_PIK IMAU JPL JPL)
+#declare -a models=(ISSM1 ISSM2 ISSM3 SICOPOLIS2 SICOPOLIS3 IMAUICE1 ISSM ISSMPALEO)
+
+# labs/models lists
+#declare -a labs=(GSFC  ILTS_PIK ILTS_PIK  JPL JPL  LSCE  MUN MUN  UCIJPL)
+#declare -a models=(ISSM SICOPOLIS2 SICOPOLIS3 ISSM ISSMPALEO GRISLI GSM2501 GSM2511 ISSM)
+
+# array sizes match
+if [ ${#labs[@]} -eq ${#models[@]} ]; then 
+    count=${#models[@]}
+else
+    echo Error: length of labs and models has to match  
+    exit 1
+fi
+
+vars="lithk orog topg sftgif sftgrf sftflf"
+
+##### 
+echo "------------------"
+echo  netcdf calculations
+echo "------------------"
+
+# loop trough labs/models
+counter=0
+while [ $counter -lt ${count} ]; do
+
+    echo ${labs[$counter]} ${models[$counter]}
+
+    proc=${labs[$counter]}_${models[$counter]}
+    mkdir -p ${proc}
+    cd ${proc}
+
+    # A. set exps manually
+    #exps_res=asmb_${ares}
+    #exps_res="ctrl_${ares} historical_${ares}"
+    exps_res="exp05_${ares}"
+    #exps_res="historical_${ares}"
+    #exps_res="ctrl_${ares}"
+    #exps_res="ctrl_proj_${ares} historical_${ares} exp05_${ares}"
+    
+    # B. find experiments automatically
+    #dexps=`find ${outp}/${labs[$counter]}/${models[$counter]}/* -maxdepth 0 -type d -name exp*`
+    #dexps=`find ${outp}/${labs[$counter]}/${models[$counter]}/* -maxdepth 0 -type d -name *_${ares}`
+    #exps_res=`basename -a ${dexps}`
+
+    echo "###"
+    echo ${exps_res}
+
+    
+    # loop trough experiments
+    for exp_res in ${exps_res}; do
+
+	apath=${outp}/${labs[$counter]}/${models[$counter]}/${exp_res}
+	# strip resolution suffix from exp
+	exp=${exp_res%???}
+
+	# output dir
+	destpath=${outpsc}/${prefix}/${labs[$counter]}/${models[$counter]}/${exp_res}
+	mkdir -p ${destpath}
+
+	# loop through variables
+	for avar in ${vars}; do
+
+	    # extract snapshots
+	    anc=${apath}/${avar}_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
+	    ncks -F -3 -O -d time,1 -v ${avar} ${anc} ${avar}_2015.nc
+	    anc=${apath}/${avar}_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
+	    ncks -F -3 -O -d time,-1 -v ${avar} ${anc} ${avar}_2100.nc
+	    # difference
+	    ncdiff ${avar}_2100.nc ${avar}_2015.nc d_${avar}_2100.nc
+
+	    # move to Archive
+	    [ -f ./${avar}_2015.nc ] && /bin/mv ./${avar}_2015.nc ${destpath}/${avar}_2015.nc_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
+	    [ -f ./${avar}_2100.nc ] && /bin/mv ./${avar}_2100.nc ${destpath}/${avar}_2100.nc_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
+	    [ -f ./d_${avar}_2100.nc ] && /bin/mv ./d_${avar}_2100.nc ${destpath}/d_${avar}_2100.nc_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
+
+	done
+	# end var loop
+
+    done
+    # end exp loop
+    
+    counter=$(( counter+1 )) 
+    
+    # back to top level directory
+    cd ../
+done
+# end lab/model loop
+
