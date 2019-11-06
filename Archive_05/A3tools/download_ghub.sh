ca502befb29be1de94d86942eadff09d95d4aed1
hgoelzer <heiko@goelzers.de>
2019-11-06 20:11:46 +0100

    Adding more scripts to consolidate Archive_05 and work with it

---
+++ Archive_05/A3tools/download_ghub.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# Download files from Archive A1
+
+# We try to correct minor filename problems by specifying the correct target name
+# See alos setting <flg_resadd> for cases where the resolution suffix is missing
+
+# Can be run to depends on a directory structure already in place. 
+# In that case we update files in the existing directory structure 
+
+# Or specify groups/models manually 
+
+
+# UB credentials
+HOST='transfer.ccr.buffalo.edu'
+USER='hgoelzer'
+
+# location of Archive
+# Mac Heiko
+#outp=/Volumes/ISMIP6/ISMIP6-Greenland/Archive/Data
+# Lisa/Cartesius
+outp=/home/hgoelzer/Projects/ISMIP6/Archive_05/Data
+
+# Remote path
+RPATH=/projects/grid/ghub/ISMIP6/Projections/GrIS/output
+
+
+# labs list
+declare -a labs=(ILTS_PIK ILTS_PIK LSCE)
+# models list
+declare -a models=(SICOPOLIS2 SICOPOLIS3 GRISLI)
+#explist="ctrl_proj_05 exp05_05"
+explist="historical_05"
+
+
+# variables
+#vars="sftgrf"
+vars="lithk orog topg sftflf sftgif sftgrf"
+#vars="lithk"
+#vars="orog topg sftflf sftgif sftgrf"
+
+# array sizes match
+if [ ${#labs[@]} -eq ${#models[@]} ]; then 
+    count=${#models[@]}
+else
+    echo Error: length of labs and models has to match  
+    exit 1
+fi
+
+##### 
+echo "------------------"
+echo  Downloading files 
+echo "------------------"
+
+# loop trough labs/models
+counter=0
+while [ $counter -lt ${count} ]; do
+
+    echo ${labs[$counter]}
+    ## find experiments
+    #dexps=`find ${outp}/${labs[$counter]}/${models[$counter]}/* -maxdepth 0 -type d `
+    #echo ${dexps}
+    #exps_res=`basename -a ${dexps}`
+    #echo "###"
+    #echo ${exps_res}
+
+    # specify exps manually
+    exps_res=${explist}
+    echo ${exps_res}
+
+    # loop trough experiments    
+    for exp_res in ${exps_res}; do
+
+	mkdir -p ${outp}/${labs[$counter]}/${models[$counter]}/${exp_res}/
+	cd ${outp}/${labs[$counter]}/${models[$counter]}/${exp_res}/
+	
+	# strip resolution suffix from exp
+	exp=${exp_res%???}
+
+	# exname is directory on server
+	if [ "$flg_resadd" = true ]; then
+	    expname=${exp}
+	else
+	    expname=${exp_res}
+	fi
+	#   # loop trough vars
+	for var in ${vars}; do
+
+	    # this is the target name, but the file on the ftp may be different
+	    anc=${var}_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
+
+	    echo ${anc}
+
+	    # ftp transaction; Allow generous interpretation of filename
+	    # as long the the varname is correct
+	    scp -i ~/.ssh/id_rsa_ghub ${HOST}:${RPATH}/${labs[$counter]}/${models[$counter]}/${expname}/${var}_*.nc ${outp}/${labs[$counter]}/${models[$counter]}/${exp_res}/${anc}
+
+	    
+	done
+	#   # end vars loop
+
+    done
+#   # end exp loop
+    
+    counter=$(( counter+1 )) 
+done
+# end lab/model loop
+
---
+++ Archive_05/A3tools/meta_check.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# checks and corrections
+
+# check something
+
+#set -x
+
+# location of Archive
+#outp=/Volumes/ISMIP6/ISMIP6-Greenland/Archive_05/Data
+outp=/home/hgoelzer/Projects/ISMIP6/Archive_05/Data
+
+# labs list
+#declare -a labs=(AWI AWI AWI)
+#declare -a models=(ISSM1 ISSM2 ISSM3)
+
+## labs list
+#declare -a labs=(BGC)
+#declare -a models=(BISICLES)
+
+## labs list
+#declare -a labs=(UCIJPL)
+#declare -a models=(ISSM1)
+
+
+declare -a labs=(AWI  AWI AWI BGC GSFC ILTS_PIK ILTS_PIK IMAU IMAU JPL JPL LSCE UAF UCIJPL)
+declare -a models=(ISSM1 ISSM2 ISSM3 BISICLES ISSM SICOPOLIS2 SICOPOLIS3 IMAUICE1 IMAUICE2 ISSM ISSMPALEO GRISLI PISM1 ISSM1)
+
+
+# array sizes match
+if [ ${#labs[@]} -eq ${#models[@]} ]; then 
+    count=${#models[@]}
+else
+    echo Error: length of labs and models has to match  
+    exit 1
+fi
+
+vars="lithk orog topg sftgif sftgrf sftflf"
+
+##### 
+echo "------------------"
+echo  netcdf corrections
+echo "------------------"
+
+# loop trough labs/models
+counter=0
+while [ $counter -lt ${count} ]; do
+
+    echo ${labs[$counter]} ${models[$counter]}
+    # set exps manually
+    #exps_res=asmb_05
+    #exps_res="ctrl_05 hist_05"
+    #exps_res="exp05_05"
+    #exps_res="hist_05"
+    
+    # find experiments
+    dexps=`find ${outp}/${labs[$counter]}/${models[$counter]}/* -maxdepth 0 -type d -name *_05`
+    exps_res=`basename -a ${dexps}`
+
+    echo "###"
+    echo ${exps_res}
+
+    
+    # loop trough experiments 
+    for exp_res in ${exps_res}; do
+
+	apath=${outp}/${labs[$counter]}/${models[$counter]}/${exp_res}
+	# strip resolution suffix from exp
+	exp=${exp_res%???}
+
+        # loop through variables
+	for avar in ${vars}; do
+
+	    # input file name
+	    anc=${apath}/${avar}_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
+	    echo ${anc}
+	    # make variable float
+	    # get length of record
+	    lt=`ncap2 -v -O -s 'print(time.size(),"%ld\n");' ${anc} foo.nc`
+	    lx=`ncap2 -v -O -s 'print(x.size(),"%ld\n");' ${anc} foo.nc`
+	    ly=`ncap2 -v -O -s 'print(y.size(),"%ld\n");' ${anc} foo.nc`
+	    echo $lt $lx $ly
+	done
+	# end var loop
+
+    done
+    # end exp loop
+    
+    counter=$(( counter+1 )) 
+done
+# end lab/model loop
+
