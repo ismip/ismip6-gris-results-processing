104ca6be335260a785a981febb2994598497d6f4
hgoelzer <heiko@goelzers.de>
2019-12-22 13:15:23 +0100

    Remove bias in ctrl corrected results by default

--- Archive_sc/A4tools/meta_scalar_proj_diffcr_05.sh
+++ Archive_sc/A4tools/meta_scalar_proj_diffcr_05.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Calculate scalar differences proj-control for a number of models/experiments
+# Subtract first entry from entire time series
 # requires a full scalar archive with ctrl_proj and exps 
 
 set -x
@@ -11,8 +12,21 @@ outpsc=/home/hgoelzer/Projects/ISMIP6/Archive_sc/Data/SC_GIC1_OBS0
 ## Settings
 ares=05
 
+## Remove bias
+flg_rb=true
+
 # or source default labs list
 source ./set_default.sh
+
+# Override default
+#declare -a labs=(LSCE)
+#declare -a models=(GRISLI2)
+#exps_res="expb04_05"
+
+# Override default
+#declare -a labs=(UCIJPL)
+#declare -a models=(ISSM1)
+#exps_res="expb01_05"
 
 # array sizes match
 if [ ${#labs[@]} -eq ${#models[@]} ]; then 
@@ -23,6 +37,7 @@ else
 fi
 
 # Define files to process
+#files="scalars_mm"
 files="scalars_mm scalars_rm"
 
 ##### 
@@ -43,10 +58,11 @@ while [ $counter -lt ${count} ]; do
     #exps_res="historical_${ares}"
     #exps_res="ctrl_${ares}"
     #exps_res="ctrl_proj_${ares} historical_${ares} exp05_${ares}"
-
     #exps_res="ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05 exp09_05 exp10_05"
 
-    #exps_res="ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05 exp09_05"
+    #exps_res="expa01_05 expa02_05 expa03_05"
+    
+    #exps_res="expb01_05 expb02_05 expb03_05 expb04_05 expb05_05"
 
     # B. find experiments automatically
     #dexps=`find ${outp}/${labs[$counter]}/${models[$counter]}/* -maxdepth 0 -type d -name exp*`
@@ -75,6 +91,20 @@ while [ $counter -lt ${count} ]; do
 	    anc_diff=${destpath}/${afile}_cr_GIS_${labs[$counter]}_${models[$counter]}_${exp}.nc
 	    # difference to ctrl_proj
 	    ncdiff -O ${anc_exp} ${anc_ctrl} anc_cr_tmp.nc
+
+	    if ($flg_rb); then
+		# Remove offset
+		/bin/cp anc_cr_tmp.nc anc_cr_tmp0.nc
+		
+		# Get vars
+		vars=`ncks -m anc_cr_tmp0.nc  | grep "1 dimension" | grep -v time | awk -F ':' '{print $1}'`
+		echo $vars
+		for avar in $vars; do
+		    ncap2 -O -s "*var_tmp=${avar}(0); ${avar}=float(${avar}-var_tmp)" anc_cr_tmp0.nc anc_cr_tmp0.nc
+		done
+		/bin/mv anc_cr_tmp0.nc anc_cr_tmp.nc
+	    fi
+
 	    # add back constants
 	    ncks -A -v oarea,rhof,rhoi,rhow,dx,dy ${anc_exp} anc_cr_tmp.nc
 	    # move to Archive
--- Archive_sc/A4tools/set_default.sh
+++ Archive_sc/A4tools/set_default.sh
@@ -29,8 +29,11 @@
 #declare -a labs=(UCIJPL UCIJPL)
 #declare -a models=(ISSM1 ISSM2)
 
-declare -a labs=(UAF)
-declare -a models=(PISM1)
+#declare -a labs=(UCIJPL)
+#declare -a models=(ISSM2)
+
+#declare -a labs=(UAF)
+#declare -a models=(PISM1)
 
 #declare -a labs=(GSFC UAF)
 #declare -a models=(ISSM PISM1)
@@ -39,16 +42,23 @@ declare -a models=(PISM1)
 #declare -a labs=(UAF UCIJPL VUW)
 #declare -a models=(PISM1 ISSM2 PISM)
 
+#declare -a labs=(VUW)
+#declare -a models=(PISM)
+
+#declare -a labs=(UCIJPL)
+#declare -a models=(ISSM1)
+
 # NOISM
 #declare -a labs=(ISMIP6 ISMIP6 ISMIP6 ISMIP6)
 #declare -a models=(NOISM_adog NOISM_adg NOISM_ag NOISM_og)
 
 ##################################
 
-#exps_res="historical_05 ctrl_proj_05"
+#exps_res="historical_05"
+#exps_res="exp05_05"
 
 # core scalars
-exps_res="historical_05 ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05 exp09_05 exp10_05"
+#exps_res="historical_05 ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05 exp09_05 exp10_05"
 
 # open core scalars
 #exps_res="historical_05 ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05"
@@ -62,3 +72,30 @@ exps_res="historical_05 ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05 exp09_0
 
 # core meta_scalar_proj_diffcr_05.sh PISM
 #exps_res="ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05 exp09_05
+
+
+##################################
+## Processing A - CMIP5
+#declare -a labs=(AWI AWI AWI BGC GSFC ILTS_PIK ILTS_PIK IMAU JPL LSCE NCAR UAF UAF UCIJPL VUB)
+#declare -a models=(ISSM1 ISSM2 ISSM3 BISICLES ISSM SICOPOLIS2 SICOPOLIS3 IMAUICE2 ISSM GRISLI2 CISM PISM1 PISM2 ISSM1 GISMSIAv3)
+
+#declare -a labs=(JPL JPL)
+#declare -a models=(ISSM ISSMPALEO)
+
+#declare -a labs=(UAF AWI)
+#declare -a models=(PISM1 ISSM1)
+
+#
+## A extension scalars
+#exps_res="expa01_05 expa02_05 expa03_05"
+#
+## B extension scalars
+#exps_res="expb01_05 expb02_05 expb03_05 expb04_05 expb05_05"
+#exps_res="expb01_05 expb02_05 expb03_05 expb05_05"
+
+
+#############################
+# All in
+declare -a labs=(AWI AWI AWI BGC GSFC ILTS_PIK ILTS_PIK IMAU IMAU JPL JPL LSCE MUN MUN NCAR UAF UAF UCIJPL UCIJPL VUB VUW)
+declare -a models=(ISSM1 ISSM2 ISSM3 BISICLES ISSM SICOPOLIS2 SICOPOLIS3 IMAUICE1 IMAUICE2 ISSM ISSMPALEO GRISLI2 GSM2601 GSM2611 CISM PISM1 PISM2 ISSM1 ISSM2 GISMSIAv3 PISM)
+exps_res="ctrl_proj_05 exp05_05 exp06_05 exp07_05 exp08_05 exp09_05 exp10_05    expa01_05 expa02_05 expa03_05    expb01_05 expb02_05 expb03_05 expb04_05 expb05_05"
